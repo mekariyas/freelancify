bf2645537324cfe030617ea771a08d5f89ac41c2
Mekariyas <mekariyas05@gmail.com>
2025-04-29 13:38:03 +0300

    setup basic express and mongoose connections

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
 import express from 'express'
+import dotenv from "dotenv"
+import cors from 'cors'
+import connectDb from './db/db.js'
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 5000
+app.use(cors())
+connectDb().catch(error=>console.error(error))
+
+
+app.get("/",(req, res)=>{
+    res.status(200).json({"message":"Hello World"})
+})
+
+app.listen(port,()=>{
+    console.log(`server running on port:${port}`)
+})
